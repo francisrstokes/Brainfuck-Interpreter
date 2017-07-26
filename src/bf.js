886eab809b13618e6218f354bd67857a9dc32b9f
Frank Stokes <frank@wearereasonablepeople.com>
2017-07-26 20:18:41 +0200

    Cleanup

--- src/bf.js
+++ src/bf.js
@@ -9,6 +9,7 @@ readFileAsync(process.argv[2], 'utf8')
   .then((file) => {
     const parser = require('./parser').parser;
     const tree = parser.parse(file);
-    const run = require('./interpreter')(tree);
-    run();
+    const run = require('./interpreter');
+
+    run(tree);
   }, console.error);
--- src/interpreter.js
+++ src/interpreter.js
@@ -6,53 +6,60 @@ let dataPointer = 0;
 let instructionPointer = -1;
 let branch = [];
 
+const checkIfBranchOff = () => {
+  if (branch[instructionPointer] && typeof branch[instructionPointer] !== 'string') {
+    const stackItem = [instructionPointer, [...branch]];
+    stack.push(stackItem);
+    branch = branch[instructionPointer];
+    instructionPointer = 0;
+  }
+};
+
+const checkIfBranchReturn = () => {
+  if (instructionPointer === branch.length) {
+    if (memory[dataPointer] === 0) {
+      [instructionPointer, branch] = stack.pop();
+    } else {
+      instructionPointer = -1;
+    }
+    return true;
+  }
+  return false;
+};
+
+const step = async function () {
+  instructionPointer++;
+  if (stack.length === 0 && !branch[instructionPointer]) return true;
+
+  checkIfBranchOff();
+  if (checkIfBranchReturn()) return false;
+
+  switch (branch[instructionPointer]) {
+    case '>':
+      dataPointer++;
+      break;
+    case '<':
+      dataPointer--;
+      break;
+    case '+':
+      memory[dataPointer]++;
+      break;
+    case '-':
+      memory[dataPointer]--;
+      break;
+    case '.':
+      process.stdout.write(String.fromCharCode(memory[dataPointer]));
+      break;
+    case ',':
+      memory[dataPointer] = await getByte();
+      break;
+  }
+
+  return false;
+};
+
 module.exports = (tree) => {
   branch = [...tree];
-
-  const step = async function () {
-    instructionPointer++;
-
-    if (stack.length === 0 && !branch[instructionPointer]) {
-      return true;
-    } else if (branch[instructionPointer] && typeof branch[instructionPointer] !== 'string') {
-      const stackItem = [instructionPointer, [...branch]];
-      stack.push(stackItem);
-      branch = branch[instructionPointer];
-      instructionPointer = 0;
-    }
-
-    const ins = branch[instructionPointer];
-    if (instructionPointer === branch.length) {
-      if (memory[dataPointer] === 0) {
-        [instructionPointer, branch] = stack.pop();
-      } else {
-        instructionPointer = -1;
-      }
-    } else {
-      switch (ins) {
-        case '>':
-          dataPointer++;
-          break;
-        case '<':
-          dataPointer--;
-          break;
-        case '+':
-          memory[dataPointer]++;
-          break;
-        case '-':
-          memory[dataPointer]--;
-          break;
-        case '.':
-          process.stdout.write(String.fromCharCode(memory[dataPointer]));
-          break;
-        case ',':
-          memory[dataPointer] = await getByte();
-          break;
-      }
-    }
-
-    return false;
-  };
 
   const run = () => {
     step().then(done => {
@@ -61,5 +68,5 @@ module.exports = (tree) => {
     });
   }
 
-  return run;
+  run();
 }
